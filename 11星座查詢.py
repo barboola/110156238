d2505b6f97ca58973773bdb44b86dc9a44af1ab8
unknown <c110156238@gmail.com>
2022-04-13 23:21:28 +0800

    first commit

---
+++ 11星座查詢.py
@@ -0,0 +1,61 @@
+list1 = input("輸入月及日為：")
+if int(list1[0]) == 1 :
+    if int(list1[1]) <= 20 :
+        print("摩羯座")
+    else :
+        print("水瓶座")
+elif int(list1[0]) == 2 :
+    if int(list1[1]) <= 18 :
+        print("水瓶座")
+    else :
+        print("雙魚座")
+elif int(list1[0]) == 3 :
+    if int(list1[1]) <= 20 :
+        print("雙魚座")
+    else :
+        print("牡羊座")
+elif int(list1[0]) == 4 :
+    if int(list1[1]) <= 20 :
+        print("牡羊座")
+    else :
+        print("金牛座")
+elif int(list1[0]) == 5 :
+    if int(list1[1]) <= 21 :
+        print("金牛座")
+    else :
+        print("雙子座")
+elif int(list1[0]) == 6 :
+    if int(list1[1]) <= 21 :
+        print("雙子座")
+    else :
+        print("巨蟹座")
+elif int(list1[0]) == 7 :
+    if int(list1[1]) <= 22 :
+        print("巨蟹座")
+    else :
+        print("獅子座")
+elif int(list1[0]) == 8 :
+    if int(list1[1]) <= 23 :
+        print("獅子座")
+    else :
+        print("處女座")
+elif int(list1[0]) == 9 :
+    if int(list1[1]) <= 23 :
+        print("處女座")
+    else :
+        print("天秤座")
+elif int(list1[0]) == 10 :
+    if int(list1[1]) <= 23 :
+        print("天秤座")
+    else :
+        print("天蠍座")
+elif int(list1[0]) == 11 :
+    if int(list1[1]) <= 22 :
+        print("天蠍座")
+    else :
+        print("射手座")
+elif int(list1[0]) == 12 :
+    if int(list1[1]) <= 21 :
+        print("射手座")
+    else :
+        print("摩羯座")
